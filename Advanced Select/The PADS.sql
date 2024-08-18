5b59daa15041c7098f1249899d5e6ba58da444cd
Aniketa Kumari <113281016+aniketakumari@users.noreply.github.com>
2024-08-18 14:40:03 +0530

    Create The PADS.sql

---
+++ Advanced Select/The PADS.sql
@@ -0,0 +1,10 @@
+-- ms mysql server
+
+select concat(name, '(', upper(left(occupation,1)),')')
+from occupations
+order by name;
+
+select concat('There are a total of ', count(occupation), ' ', lower(occupation), 's.')
+from occupations
+group by occupation 
+order by count(occupation), occupation;
