3e3e8476e7298b89a4b70dae229fd3d52bd658ca
Aniketa Kumari <113281016+aniketakumari@users.noreply.github.com>
2024-08-22 13:13:03 +0530

    Update The PADS.sql

--- Advanced Select/The PADS.sql
+++ Advanced Select/The PADS.sql
@@ -8,3 +8,15 @@ select concat('There are a total of ', count(occupation), ' ', lower(occupation)
 from occupations
 group by occupation 
 order by count(occupation), occupation;
+
+
+-- mysql
+
+SELECT CONCAT(Name, '(', LEFT(Occupation, 1) , ')')
+FROM Occupations 
+ORDER BY Name;
+
+SELECT CONCAT('There are a total of ', COUNT(*), ' ', LOWER(Occupation), 's.')
+FROM Occupations 
+GROUP BY Occupation
+ORDER BY COUNT(Occupation) ASC, Occupation;
